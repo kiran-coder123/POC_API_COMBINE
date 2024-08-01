15760a23c1f07c61e0624f92282519e21635e806
Kiran Sonne <A13221KS@partner.bajajfinserv.in>
2024-08-01 22:25:59 +0530

    Initial Commit

---
+++ POC_API_COMBINE/POC_API_COMBINEApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_API_COMBINEApp.swift
+//  POC_API_COMBINE
+//
+//  Created by Kiran Sonne on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct POC_API_COMBINEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
